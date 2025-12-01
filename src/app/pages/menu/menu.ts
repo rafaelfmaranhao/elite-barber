1f90eb7df42cd5618ea3a40b7098460aa8826652
lipdzy <lipdzy@gmail.com>
2025-12-01 19:33:28 -0300

    fix: botao agendar do header

--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -4,10 +4,12 @@ import { RouterLink } from '@angular/router';
 import { Cortes } from "../../components/menu-sections/cortes/cortes";
 import { Barbas } from "../../components/menu-sections/barbas/barbas";
 import { Luzes } from "../../components/menu-sections/luzes/luzes";
+import { ModalAgendamentoComponent } from "../../components/modal-agendamento/modal-agendamento.component";
+import { ModalLoginComponent } from "../../components/modal-login/modal-login.component";
 
 @Component({
   selector: 'app-menu',
-  imports: [HeaderComponent, Cortes, Barbas, Luzes],
+  imports: [HeaderComponent, Cortes, Barbas, Luzes, ModalAgendamentoComponent, ModalLoginComponent],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
